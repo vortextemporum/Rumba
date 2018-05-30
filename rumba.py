ab96d3d16ad00faea599d8103d9194a25277bbb5
Berk Özdemir <berkozdemir@Berks-MBP.lan>
2018-05-30 22:56:41 +0200

    Update

--- rumba.py
+++ rumba.py
@@ -131,6 +131,16 @@ YO YO YO YO YO YO YO YO YO YO YO
         self.x = row - 1 - self.x
         self.y = row - 1 - self.y
         # return [self.x,self.y]
+
+    def swapXY(self):
+        # Not Tested
+        x = self.x
+        y = self.y
+
+        self.x = y
+        self.y = x
+        # Check if there is any need of such thing
+        pass
     
 
     #SEQUENCE TRANSFORMATIONS
@@ -158,12 +168,19 @@ YO YO YO YO YO YO YO YO YO YO YO
         self.sequence = self.sequence[::-1]
        # return self.sequence
 
+    def swapXYAll(self):
+         # Not Tested
+        self.sequence = [(t[1], t[0]) for t in self.sequence]
+        pass
+
     def moveAll(self, x=0, y=0):
+         # Not Tested
         for couple in self.sequence:
             couple[0] = couple[0] + x
             couple[1] = couple[1] + y
 
     def shuffle(self):
+         # Not Tested
         self.sequence = random.shuffle(self.sequence)
     
     def reset(self):
@@ -228,6 +245,7 @@ def doubleSpiral(n):
     return seq1.sequence, seq2.sequence
 
 def Square(n, corner=4):
+    # Not Tested
     seq = Grid(x=0,y=0,addToList=True,direction=6)
     
     for i in range(corner):
@@ -237,7 +255,7 @@ def Square(n, corner=4):
 
 
 def Spiral(n):
-
+    # Not Tested
     seq = Grid(x=-1,y=0,addToList=False,direction=6)
     
     for newn in range(rows, 2, -2):
@@ -252,7 +270,7 @@ def Spiral(n):
     return seq
 
 def Regular(n):
-
+     # Not Tested
     seq = Grid(addToList=False)
 
     for column in range (n):
@@ -261,13 +279,13 @@ def Regular(n):
     return seq
 
 def Shuffle(n):
-
+     # Not Tested
     return Regular(n).shuffle
 
 
 def Karo(n):
-    seq = Grid(x=int(n/2)-1, y=0, addToList=False, direction= 7)
-    
+     # Not Tested
+    seq = Grid(x=int(n/2)-1, y=0, addToList=False, direction=7)
     
     if n % 2 == 0:
         for i in range(n/2-1, 0, -1):
@@ -291,13 +309,9 @@ def Karo(n):
             seq.turn(3,1)
 
     return seq
-
-'''
-ADD ODD NUMBER
-'''
         
 def Z(n=5, corners = 3):
-
+     # Not Tested
     seq = Grid(x=0,y=0,addToList=True,direction=5)
 
     for i in range(corners):
@@ -308,13 +322,32 @@ def Z(n=5, corners = 3):
 
     return seq
 
-def Snake(n):
-  
-    seq = Grid()
-    seq.turn(2,1)
-    seq.turn(2,n-1)
-    pass
+def Snake(n, repeat=1):
+     # Not Tested
+    seq = Grid(x=0,y=-1, addToList=False  direction=4)
+
+    for i in range(repeat):
+        for column in range(n):
+            
+            if i % 2 == 0:
+                seq.turn(6,1)
+                seq.turn(6,n-1)
+
+            elif i % 2 == 1:
+                seq.turn(2,1)
+                seq.turn(2,n-1)
+
+    return seq
+
+'''
+DRAW ALPHABET START
+'''
 
 def LetterB(n):
 
     pass
+
+
+'''
+DRAW ALPHABET END
+'''
